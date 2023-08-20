57652c12b93c8f11449e880854128340cd327c96
Dylan Kirby <dylan.kirby.365@gmail.com>
2023-08-20 17:24:18 +0200

    Fix move of temp

--- test.py
+++ test.py
@@ -239,15 +239,15 @@ def main():
 
     if args['--save'] is not None:
         try:
-            shutil.move('temp', args['--save'])
+            shutil.move('temp/*', args['--save'])
             cprint(f'Test results saved to {args["--save"]}!', 'green')
             logging.info(f'Saving test results to {args["--save"]}...')
         except Exception as e:
             cprint(f'Error saving test results: {e}', 'red')
             logging.error(f'Error saving test results: {e}')
-    else:
-        cprint('Cleaning up...', 'yellow')
-        rm_temp()
+
+    cprint('Cleaning up...', 'yellow')
+    rm_temp()
 
     cprint('Done.', 'blue')
 
