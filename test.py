c593104ff56ff36002fd2b7b15f77196164b1d8e
Dylan Kirby <dylan.kirby.365@gmail.com>
2023-08-24 15:24:45 +0200

    Complete code refactor

--- test.py
+++ test.py
@@ -31,239 +31,276 @@ import os
 import shutil
 import signal
 import subprocess
-from typing import List
+import sys
 
 from docopt import docopt
-from termcolor import cprint
 
-from utils import compile_test_module
-
-# Basic logging configuration
-logging.basicConfig(level=logging.INFO)
+CWD = os.getcwd().replace(' ', '\\ ')
+"""The current working directory, formatted for use in shell commands."""
 
 
-def execute_test(
-        exe_name: str,
-        test_number: int,
-        bin_dir: str,
-        test_dir: str,
-        should_valgrind: bool = True,
-        timeout: float = 10
-) -> bool:
+class CustomFormatter(logging.Formatter):
     """
-    Executes the test for the specified module and test number.
-
-    :param exe_name: The name of the module's binary file (testscanner, amplc, testhashtable, testsymboltable)
-    :param test_number: The test number to execute
-    :param bin_dir: The directory containing the test executables
-    :param test_dir: The directory containing the test files
-    :param should_valgrind: Whether to perform a memory check (default: True)
-    :param timeout: The timeout in seconds (default: 10)
-
-    :return: True if the test executed, False otherwise
+    Custom logging formatter for coloured output.
     """
 
-    stdout_path = f'temp/{test_number}.out'
-    stderr_path = f'temp/{test_number}.err'
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
-    with open(stdout_path, 'w') as f_out, open(stderr_path, 'w') as f_err:
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
 
-        process = subprocess.Popen(
-            [f'{bin_dir}/{exe_name}', f'{test_dir}/{test_number}.ampl'],
-            stdout=f_out,
-            stderr=f_err,
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+class Test:
+    """
+    A class representing a test.
+    """
+
+    def __init__(
+        self,
+        executable: str,
+        perform_mem_check: bool = True,
+        side_by_side: bool = False,
+        src_dir=f'{CWD}/../src',
+        bin_dir=f'{CWD}/../bin',
+        tests_dir=f'{CWD}/tests',
+        temp_dir=f'{CWD}/temp'
+    ) -> None:
+        """
+        Creates a new test.
+
+        :param executable: The name of the executable to test
+        :param perform_mem_check: Whether to perform a memory check on the executable
+        :param side_by_side: Whether to display the diff side by side
+        :param src_dir: The source directory
+        :param bin_dir: The binary directory
+        :param tests_dir: The tests directory
+        :param temp_dir: The temp directory
+        """
+
+        # Name
+        self._formal_name = executable
+        self._make_name = f'test{executable}'
+        self._exe_name = f'test{executable}' if executable != 'parser' else 'amplc'
+
+        # Flags
+        self._perform_mem_check = perform_mem_check
+        self._side_by_side = side_by_side
+
+        # Directories
+        self._src_dir = src_dir
+        self._bin_dir = bin_dir
+        self._tests_dir = tests_dir
+        self._temp_dir = temp_dir
+
+        # Constants
+        self._TIMEOUT = 10
+
+    def compile(self) -> bool:
+        """
+        Compiles the test.
+
+        Returns:
+            bool: True if compilation was successful, False otherwise
+        """
+
+        logging.info(f'Compiling {self._formal_name}...')
+        clean_proc = subprocess.Popen(
+            ['make', 'clean'],
+            cwd=self._src_dir,
+            stdout=subprocess.DEVNULL
+        )
+        clean_proc.wait()
+
+        comp_proc = subprocess.Popen(
+            ['make', f'{self._make_name}'],
+            cwd=self._src_dir,
+            stdout=subprocess.DEVNULL
+        )
+        comp_proc.wait()
+
+        if comp_proc.returncode == 0:
+            logging.info(
+                f'{self._formal_name} compiled successfully!')
+            return True
+
+        logging.error(
+            f'{self._formal_name} failed to compile with error code {comp_proc.returncode}')
+        return False
+
+    def exec_unit(self, test_number: int) -> bool:
+        """
+        Executes a single test case.
+
+        :param test_number: The test number to execute
+        :return: True if the test executed, False otherwise
+        """
+
+        temp_out = f'{self._temp_dir}/{test_number}.out'
+        temp_err = f'{self._temp_dir}/{test_number}.err'
+
+        with open(temp_out, 'w') as f_out, open(temp_err, 'w') as f_err:
+
+            process = subprocess.Popen(
+                [f'{self._bin_dir}/{self._exe_name}',
+                    f'{self._tests_dir}/{test_number}.ampl'],
+                stdout=f_out,
+                stderr=f_err,
+                preexec_fn=os.setsid  # Create a new process group
+            )
+
+            try:
+                process.wait(timeout=self._TIMEOUT)
+                return True
+            except subprocess.TimeoutExpired:
+                logging.warning(
+                    f'Test {test_number} timed out after {self._TIMEOUT} seconds.')
+                self._handle_timeout(process)
+                return False
+
+    def memory_check_unit(self, test_number: int) -> bool:
+
+        # Check for leaks
+        valgrind_proc = subprocess.Popen(
+            ['valgrind', '--leak-check=full', '--error-exitcode=-1',
+                f'{self._bin_dir}/{self._exe_name}', f'{self._temp_dir}/{test_number}.ampl'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
             preexec_fn=os.setsid  # Create a new process group
         )
 
         try:
-            process.wait(timeout=timeout)
+            valgrind_proc.wait(timeout=10)
+            if valgrind_proc.returncode == -1:
+                logging.error(f'Test {test_number} failed memory check')
+                return False
+            return True
         except subprocess.TimeoutExpired:
-            cprint(
-                f'Test {test_number} timed out after {timeout} seconds.', 'red')
-            handle_timeout(process, exe_name)
+            logging.warning(
+                f'Valgrind {test_number} timed out after 10 seconds.')
+            self._handle_timeout(valgrind_proc)
             return False
 
-    if not should_valgrind:
-        return True
+    def _handle_timeout(self, process: subprocess.Popen):
 
-    # Check for leaks
-    valgrind_proc = subprocess.Popen(
-        ['valgrind', '--leak-check=full', '--error-exitcode=-1',
-            f'{bin_dir}/{exe_name}', f'{test_dir}/{test_number}.ampl'],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-        preexec_fn=os.setsid  # Create a new process group
-    )
-
-    try:
-        valgrind_proc.wait(timeout=timeout)
-        if valgrind_proc.returncode == -1:
-            cprint(f'Test {test_number} failed memory check', 'red')
-            return False
-        return True
-    except subprocess.TimeoutExpired:
-        cprint(
-            f'Valgrind {test_number} timed out after {timeout} seconds.', 'red')
-        handle_timeout(valgrind_proc, exe_name)
-        return False
-
-
-def handle_timeout(process: subprocess.Popen, module: str):
-
-    try:
-        # Terminate the whole process group
-        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-        process.wait(timeout=1)
-    except subprocess.TimeoutExpired:
-        logging.warning(
-            f'{module} process could not be terminated using SIGTERM, attempting SIGKILL.'
-        )
         try:
-            os.killpg(os.getpgid(process.pid), signal.SIGKILL)
+            # Terminate the whole process group
+            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
             process.wait(timeout=1)
         except subprocess.TimeoutExpired:
-            logging.error(
-                f'{module} process could not be terminated using SIGKILL. Manual intervention required.'
+            logging.warning(
+                f'{self._exe_name} process could not be terminated using SIGTERM, attempting SIGKILL.'
             )
-            raise Exception("Test executable could not be terminated.")
+            try:
+                os.killpg(os.getpgid(process.pid), signal.SIGKILL)
+                process.wait(timeout=1)
+            except subprocess.TimeoutExpired:
+                logging.error(
+                    f'{self._exe_name} process could not be terminated using SIGKILL. Manual intervention required.'
+                )
+                raise Exception("Test executable could not be terminated.")
 
+    def diff_unit(self, test_number: int) -> bool:
+        """
+        Does a diff check on the out and err files.
 
-def diff_check(module: str, test_number: int, flags: str = '') -> bool:
-    """
-    Does a diff check on the out and err files.
+        :param test_number: The test number to diff
+        :param flags: The flags to pass to diff
+        :return: True if both diffs passed, False otherwise
+        """
 
-    :param module: The module to diff the output files on
-    :param test_number: The test number to diff
-    :param flags: The diff check flags
-    :return: True if both diffs passed, False otherwise
-    """
+        diff_flags = '-side-by-side --suppress-common-lines' if self._side_by_side else ''
 
-    passed = True
-    for output_type in ['out', 'err']:
-        diff_proc = subprocess.Popen(
-            [f'diff {flags} temp/{test_number}.{output_type} {module}/{test_number}.{output_type}'],
-            shell=True,
-            cwd=os.getcwd()
-        )
-        diff_proc.wait()
+        passed = True
+        for output_type in ['out', 'err']:
+            diff_proc = subprocess.Popen(
+                [f'diff {diff_flags} {self._temp_dir}/{test_number}.{output_type} {self._formal_name}/{test_number}.{output_type}'],
+                shell=True,
+                cwd=os.getcwd()
+            )
+            diff_proc.wait()
 
-        if diff_proc.returncode != 0:
-            cprint(f'Test {test_number} failed ({output_type}).', 'red')
-            passed = False
+            if diff_proc.returncode != 0:
+                logging.error(
+                    f'Test {test_number} failed diff check ({output_type}).')
+                passed = False
 
-    return passed
+        return passed
 
+    def test_unit(self, test_number: int) -> bool:
+        """
+        Runs the test.
 
-def run_test(
-        module: str,
-        test_numbers: List[int],
-        is_mem_check: bool = True,
-        is_side_by_side: bool = False
-) -> bool:
-    """
-    Runs the tests for the specified module and test numbers.
+        :param test_number: The test number to execute
+        :return: True if the test executed, False otherwise
+        """
 
-    :param module: The module to test (scanner, parser, hashtable, symboltable)
-    :param test_numbers: The test numbers to execute (default: [0..10])
-    :param is_mem_check: Whether to perform a memory check (default: True)
-    :param is_side_by_side: Whether to show side-by-side diff (default: False)
+        if not self.exec_unit(test_number):
+            return False
 
-    :return: True if all tests passed, False otherwise
-    """
+        if self._perform_mem_check:
+            if not self.memory_check_unit(test_number):
+                return False
 
-    diff_flags = '--side-by-side --suppress-common-lines' if is_side_by_side else ''
+        if not self.diff_unit(test_number):
+            return False
 
-    # Get the bin and test directories
-    bin_dir = os.path.join(os.getcwd(), '../bin')
-    test_dir = os.path.join(os.getcwd(), 'tests')
-
-    cprint(f'Running tests for {module}...', 'blue')
-
-    failed_tests = []
-
-    for test_number in test_numbers:
-
-        module_name = f'test{module}' if module != 'parser' else 'amplc'
-
-        if not execute_test(module_name, test_number, bin_dir,  test_dir, is_mem_check):
-            failed_tests.append(test_number)
-            continue
-
-        if diff_check(module, test_number, diff_flags):
-            cprint(f'Test {test_number} passed.', 'green')
-        else:
-            failed_tests.append(test_number)
-
-    if not failed_tests:
-        cprint(f'All tests passed for {module}!', 'green')
+        logging.info(f'Test {test_number} passed.')
         return True
 
-    cprint(f'Tests {failed_tests} failed for {module}.', 'red')
-    return False
+    def run(self, test_cases: list[int]) -> bool:
+
+        if not self.compile():
+            return False
+
+        passed = True
+        for test_number in test_cases:
+            if not self.test_unit(test_number):
+                passed = False
+
+        return passed
+
+    def save_results(self, save_dir) -> None:
+
+        if os.path.exists(save_dir):
+            shutil.rmtree(save_dir)
+
+        try:
+            shutil.move(self._temp_dir, save_dir)
+            logging.info(f'Saving test results to {save_dir}...')
+        except Exception as e:
+            logging.error(f'Error saving test results: {e}')
+
+    def rm_temp(self) -> None:
+        """Removes the temp directory."""
+
+        try:
+            shutil.rmtree(self._temp_dir)
+            logging.info('Temp directory removed successfully.')
+        except Exception as e:
+            logging.warning(f'Could not remove temp directory: {e}')
 
 
-def rm_temp():
-    """Removes the temp directory."""
-
-    temp_dir = os.path.join(os.getcwd(), 'temp')
-
-    try:
-        shutil.rmtree(temp_dir)
-        cprint('Temp directory removed successfully.', 'green')
-    except Exception as e:
-        cprint(f'Warning: Could not remove temp directory: {e}', 'yellow')
-        cprint(
-            'The temp directory may contain .nfs files, this is expected behavior.', 'yellow')
-
-
-def parse_test_cases(test_args):
+def parse_modules(args: dict[str, bool]) -> list[str]:
     """
-    Parses the test cases from the command line.
-    Either a list of integers or a start..end range can be provided.
+    Parses the modules from the command line.
     """
-
-    if not test_args:
-        # get all the available tests
-        count = len(os.listdir(os.path.join(os.getcwd(), 'tests')))
-        return list(range(0, count))
-
-    if ".." in test_args[0]:
-        start, end = map(int, test_args[0].split(".."))
-        return list(range(start, end + 1))
-    return [int(i) for i in test_args]
-
-
-def compile_and_run_tests(module, test_cases, is_mem_check, side_by_side):
-    """Compiles and runs the tests for the specified module."""
-
-    try:
-        if compile_test_module(module):
-            run_test(module, test_cases, is_mem_check, side_by_side)
-    except Exception as e:
-        cprint(f'Error running tests: {e}', 'red')
-        logging.error(f'Error: {e}')
-        return False
-    return True
-
-
-def handle_keyboard_interrupt(sig, frame):
-    """Handles keyboard interrupts."""
-
-    cprint('Keyboard interrupt detected, cleaning up...', 'yellow')
-    cprint('Warning: Please kill any lingering test executables manually.', 'red')
-    rm_temp()
-    exit(0)
-
-
-def main():
-
-    signal.signal(signal.SIGINT, handle_keyboard_interrupt)
-
-    args = docopt(__doc__)
-    test_dir = os.getcwd()
 
     modules = []
-    test_cases = range(0, 50+1)
 
     if args['scanner']:
         modules.append('scanner')
@@ -276,39 +313,79 @@ def main():
     if args['all']:
         modules = ['scanner', 'parser', 'hashtable', 'symboltable']
 
+    if len(modules) == 0:
+        logging.error('Invalid modules specified.')
+        sys.exit(1)
+
+    logging.debug(f'Modules: {modules}')
+
+    return modules
+
+
+def parse_test_cases(test_args) -> list[int]:
+    """
+    Parses the test cases from the command line.
+    Either a list of integers or a start..end range can be provided.
+    """
+    cases = []
+
+    if not test_args:
+        count = len(os.listdir(os.path.join(os.getcwd(), 'tests')))
+        cases = list(range(0, count))
+    elif ".." in test_args[0]:
+        start, end = map(int, test_args[0].split(".."))
+        cases = list(range(start, end + 1))
+    else:
+        cases = list(map(int, test_args))
+
+    logging.debug(f'Test cases: {cases}')
+    return cases
+
+
+def handle_keyboard_interrupt(sig, frame):
+    """Handles keyboard interrupts."""
+
+    logging.warning('Keyboard interrupt detected!')
+    # TODO: Terminate all processes
+    sys.exit(0)
+
+
+def main():
+
+    # Interrupt handler
+    signal.signal(signal.SIGINT, handle_keyboard_interrupt)
+
+    # Logging
+    logger = logging.getLogger("Test Executor")
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(CustomFormatter())
+    logger.addHandler(ch)
+
+    # Argument parsing
+    args = docopt(__doc__)
+    logging.debug(f'Arguments: {args}')
+    modules = parse_modules(args)
     test_cases = parse_test_cases(args['<tests>'])
 
-    if len(modules) == 0:
-        cprint('Invalid modules specified.', 'red')
-        return
-
-    cprint('Executing Setup', 'yellow')
-    os.chdir(test_dir)
+    # CWD and temp dir setup
+    logging.info('Setting up')
+    os.chdir(CWD)
+    logging.debug(f'Current working directory: {os.getcwd()}')
     os.makedirs('temp', exist_ok=True)
 
     for module in modules:
-        if not compile_and_run_tests(module, test_cases, args['--valgrind'], args['--side-by-side']):
-            break
+        test = Test(module, args['--valgrind'], args['--side-by-side'])
+        test.run(test_cases)
 
-    os.chdir(test_dir)
-    if args['--save'] is not None:
+        if args['--save'] is not None:
+            test.save_results(args['--save'])
+        else:
+            test.rm_temp()
 
-        if os.path.exists(args['--save']):
-            shutil.rmtree(args['--save'])
-
-        try:
-            shutil.move('temp', args['--save'])
-            cprint(
-                f'Test results saved to {args["--save"]} directory!', 'green')
-            logging.info(f'Saving test results to {args["--save"]}...')
-        except Exception as e:
-            cprint(f'Error saving test results: {e}', 'red')
-            logging.error(f'Error saving test results: {e}')
-    else:
-        cprint('Cleaning up...', 'yellow')
-        rm_temp()
-
-    cprint('Done.', 'blue')
+    logging.info('Done.')
 
 
 if __name__ == '__main__':
